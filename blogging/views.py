26041c9f22a21ae6765f8ff2df8298a231fb0b2c
Arun Nalla <nalla.arunkumar@gmail.com>
2019-05-21 21:05:08 -0700

    Activity Class07

--- blogging/views.py
+++ blogging/views.py
@@ -1,25 +1,47 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
-from django.http import Http404
 from blogging.models import User
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post
+
+
+# def detail_view(request, poll_id):
+#     try:
+#         poll = User.objects.get(pk=poll_id)
+#     except User.DoesNotExist:
+#         raise Http404
+
+#     if request.method == "POST":
+#         if request.POST.get("vote") == "Yes":
+#             poll.score += 1
+#         else:
+#             poll.score -= 1
+#         poll.save()
+
+#     context = {'poll': poll}
+#     return render(request, 'blogging/detail.html', context)
+
+# def stub_view(request, *args, **kwargs):
+#     body = "Stub View\n\n"
+#     if args:
+#         body += "Args:\n"
+#         body += "\n".join(["\t%s" % a for a in args])
+#     if kwargs:
+#         body += "Kwargs:\n"
+#         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+#     return HttpResponse(body, content_type="text/plain")
+
+
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     template = loader.get_template('blogging/list.html')
+#     context = {'posts': posts}
+#     body = template.render(context)
+#     return HttpResponse(body, content_type="text/html")
 
 def list_view(request):
-    context = {'users': User.objects.all()}
-    return render(request, 'blogging/list.html', context)
-
-def detail_view(request, poll_id):
-    try:
-        poll = User.objects.get(pk=poll_id)
-    except User.DoesNotExist:
-        raise Http404
-
-    if request.method == "POST":
-        if request.POST.get("vote") == "Yes":
-            poll.score += 1
-        else:
-            poll.score -= 1
-        poll.save()
-
-    context = {'poll': poll}
-    return render(request, 'blogging/detail.html', context)
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'list.html', context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include  # <-- Make sure you have both of these imports.
+from django.urls import path, include
 
 urlpatterns = [
     path('polling/', include('polling.urls')),
